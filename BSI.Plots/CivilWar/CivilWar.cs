eb6d4fc7e354f6743077a4e4269d49de87066c8d
DrunkenFrenchman <72567231+DrunkenFrenchman@users.noreply.github.com>
2020-10-12 18:58:07 -0500

    Plot Module Uploaded to Git

---
+++ BSI.Plots/CivilWar/CivilWar.cs
@@ -0,0 +1,18 @@
+﻿using BSI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+
+namespace BSI.Plots
+{
+    public class CivilWar : Plot, IPlot
+    {
+        
+    }
+}
